6cab0a51d47f3e8d7759a0fa616e97c52ee0990c
Jason Garza <gjason99@gmail.com>
2022-06-05 02:36:02 -0500

    Added code

---
+++ WordleHelper.py
@@ -0,0 +1,284 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter.scrolledtext import ScrolledText
+import enchant
+
+my_dict = enchant.Dict('en_US')
+remaining_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                     'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+required_letters = []
+word_count = 1
+
+
+def main():
+    window = tk.Tk()
+
+    window.title('Wordle Helper')
+
+    window_width = 400
+    window_height = 190
+
+    # get screen dimensions
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    # get coordinates for centering window
+    center_x = int(screen_width / 2 - window_width / 2)
+    center_y = int(screen_height / 2 - window_height / 2)
+    # position window in the middle of the screen
+    window.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+    # don't let the window be resizeable
+    window.resizable(False, False)
+
+    # set columns for window
+    window.columnconfigure(0, weight=4)
+    window.columnconfigure(1, weight=1)
+    window.columnconfigure(2, weight=4)
+
+    # create word input
+    word_frame = ttk.Frame(window)
+
+    l1_text = tk.StringVar()
+    l1_box = ttk.Entry(word_frame, width=1, textvariable=l1_text)
+    l1_text.trace('w', lambda *args: character_limit(l1_text))
+    l1_box.grid(column=0, row=0)
+
+    l2_text = tk.StringVar()
+    l2_box = ttk.Entry(word_frame, width=1, textvariable=l2_text)
+    l2_text.trace('w', lambda *args: character_limit(l2_text))
+    l2_box.grid(column=1, row=0)
+
+    l3_text = tk.StringVar()
+    l3_box = ttk.Entry(word_frame, width=1, textvariable=l3_text)
+    l3_text.trace('w', lambda *args: character_limit(l3_text))
+    l3_box.grid(column=2, row=0)
+
+    l4_text = tk.StringVar()
+    l4_box = ttk.Entry(word_frame, width=1, textvariable=l4_text)
+    l4_text.trace('w', lambda *args: character_limit(l4_text))
+    l4_box.grid(column=3, row=0)
+
+    l5_text = tk.StringVar()
+    l5_box = ttk.Entry(word_frame, width=1, textvariable=l5_text)
+    l5_text.trace('w', lambda *args: character_limit(l5_text))
+    l5_box.grid(column=4, row=0)
+
+    word_frame.grid(row=0, column=0, pady=10)
+
+    # create label for letters to remove
+    remove_label = ttk.Label(window, text="Not in the word:")
+    remove_label.grid(row=1, column=0, sticky=tk.W, padx=10)
+
+    # create textbox for letters to remove
+    remove_text = tk.StringVar()
+    remove_box = ttk.Entry(window, textvariable=remove_text)
+    remove_text.trace('w', lambda *args: make_upper(remove_text))
+    remove_box.grid(row=2, column=0, sticky=tk.W, padx=10)
+
+    # create label for required letters
+    required_label = ttk.Label(window, text="In the word somewhere:")
+    required_label.grid(row=3, column=0, sticky=tk.W, padx=10)
+
+    # create textbox for letters to remove
+    required_text = tk.StringVar()
+    required_box = ttk.Entry(window, textvariable=required_text)
+    required_text.trace('w', lambda *args: make_upper(required_text))
+    required_box.grid(row=4, column=0, sticky=tk.W, padx=10)
+
+    # create button for running program
+    run_button = ttk.Button(window, text='Find Words', command=lambda: find_words(l1_text.get(), l2_text.get(), l3_text.get(), l4_text.get(), l5_text.get(), remove_text.get(), required_text.get(), results))
+    run_button.grid(row=5, column=0, pady=10)
+
+    # create text widget for the results
+    results = ScrolledText(window, height=13, width=20)
+    results.grid(row=0, column=2, rowspan=6, padx=10)
+    results['state'] = 'disabled'
+
+    window.mainloop()
+
+
+def find_words(l1, l2, l3, l4, l5, remove, required, results):
+    global remaining_letters
+    global required_letters
+    global word_count
+
+    # reset remaining letters if needed
+    if len(remaining_letters) != 26:
+        remaining_letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                             'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    # reset required letters if needed
+    if len(required_letters) > 0:
+        required_letters = []
+
+    # reset word count
+    word_count = 1
+
+    # clear text box
+    results['state'] = 'normal'
+    results.delete('1.0', 'end')
+    results['state'] = 'disabled'
+
+    # get word from the 5 input letters
+    word = make_word(l1, l2, l3, l4, l5)
+
+    # remove letters that are know to not be in word
+    for letter in remove:
+        if letter in remaining_letters:
+            remaining_letters.remove(letter)
+
+    # get letters that must be in word
+    for letter in required:
+        required_letters.append(letter)
+
+    # get number of missing letters
+    num_missing = word.count('_')
+
+    # generate possible words
+    if num_missing == 1:
+        missing_1(word, results)
+    elif num_missing == 2:
+        missing_2(word, results)
+    elif num_missing == 3:
+        missing_3(word, results)
+    elif num_missing == 4:
+        missing_4(word, results)
+    elif num_missing == 5:
+        missing_5(results)
+
+
+def missing_1(word, results):
+    # for each remaining letter
+    for let in remaining_letters:
+        new_word = ''
+        for old_let in word:
+            if old_let == '_':
+                new_word += let
+            else:
+                new_word += old_let
+        check_and_add(new_word, results)
+
+
+def missing_2(word, results):
+    # for all possible letter combinations
+    for let_1 in remaining_letters:
+        for let_2 in remaining_letters:
+            new_word = ''
+            count = 0
+            # create new word with letters substituted in
+            for let in word:
+                if let == '_' and count == 0:
+                    new_word += let_1
+                    count += 1
+                elif let == '_':
+                    new_word += let_2
+                else:
+                    new_word += let
+            check_and_add(new_word, results)
+
+
+def missing_3(word, results):
+    # for all possible letter combinations
+    for let_1 in remaining_letters:
+        for let_2 in remaining_letters:
+            for let_3 in remaining_letters:
+                new_word = ''
+                count = 0
+                # create new word with letters substituted in
+                for let in word:
+                    if let == '_' and count == 0:
+                        new_word += let_1
+                        count += 1
+                    elif let == '_' and count == 1:
+                        new_word += let_2
+                        count += 1
+                    elif let == '_':
+                        new_word += let_3
+                    else:
+                        new_word += let
+                check_and_add(new_word, results)
+
+
+def missing_4(word, results):
+    # for all possible letter combinations
+    for let_1 in remaining_letters:
+        for let_2 in remaining_letters:
+            for let_3 in remaining_letters:
+                for let_4 in remaining_letters:
+                    new_word = ''
+                    count = 0
+                    # create new word with letters substituted in
+                    for let in word:
+                        if let == '_' and count == 0:
+                            new_word += let_1
+                            count += 1
+                        elif let == '_' and count == 1:
+                            new_word += let_2
+                            count += 1
+                        elif let == '_' and count == 2:
+                            new_word += let_3
+                            count += 1
+                        elif let == '_':
+                            new_word += let_4
+                        else:
+                            new_word += let
+                    check_and_add(new_word, results)
+
+
+def missing_5(results):
+    results['state'] = 'normal'
+    results.insert('1.0', 'Please enter at')
+    results.insert('2.0', 'least one letter')
+    results['state'] = 'disabled'
+
+
+def check_and_add(word, results):
+    global word_count
+    valid = True
+    # check if word exists
+    if not my_dict.check(word):
+        valid = False
+    # check if word contains required letters
+    for letter in required_letters:
+        if letter not in word:
+            valid = False
+    # add word to textbox if it is valid
+    if valid:
+        results['state'] = 'normal'
+        results.insert(f'{word_count}.0', f'{word}\n')
+        results['state'] = 'disabled'
+        word_count += 1
+
+
+def make_word(l1, l2, l3, l4, l5):
+    if l1 == '':
+        l1 = '_'
+    if l2 == '':
+        l2 = '_'
+    if l3 == '':
+        l3 = '_'
+    if l4 == '':
+        l4 = '_'
+    if l5 == '':
+        l5 = '_'
+    return l1 + l2 + l3 + l4 + l5
+
+
+def character_limit(text):
+    # only let user type letters
+    if not text.get().isalpha():
+        text.set('')
+    # if user entered more than 1 letter, remove extra letters
+    elif len(text.get()) > 0:
+        text.set(text.get()[0].upper())
+
+
+def make_upper(text):
+    # if user entered non-alpha character, remove it
+    if len(text.get()) > 0 and not text.get()[-1].isalpha():
+        text.set(text.get()[:-1].upper())
+    else:
+        text.set(text.get().upper())
+
+
+if __name__ == '__main__':
+    main()
